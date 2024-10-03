f46e23c54b5dafe5770a14c2b560535dcfaeb421
Haidar Priatama <69595585+haidarpriatama@users.noreply.github.com>
2024-10-03 11:30:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,22 +7,3 @@ def tampilkan_menu():
     pilihan = int(input("Pilih opsi: "))
     return pilihan
 
-if __name__ == "__main__":
-    jadwal_kuliah = JadwalKuliah()
-
-    while True:
-        pilihan = tampilkan_menu()
-
-        if pilihan == 1:
-            hari = input("\nMasukkan hari: ")
-            mata_kuliah = input("Masukkan mata kuliah: ")
-            jadwal_kuliah.tambah_jadwal(hari, mata_kuliah)
-
-        elif pilihan == 2:
-            jadwal_kuliah.tampilkan_jadwal()
-
-        elif pilihan == 3:
-            print("\nProgram selesai.")
-            break
-        else:
-            print("\nPilihan tidak valid, coba lagi.")
